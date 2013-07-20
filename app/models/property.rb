eb0d23b7d5934b9e05dc3efce75cbce0495deba8
Jean Brito <jeanfbrito@gmail.com>
2013-07-20 14:42:12 -0300

    create get_cover function to properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,4 +4,12 @@ class Property < ActiveRecord::Base
   has_many :pictures
   belongs_to :district
 
+  def get_cover
+  	cover = pictures.where(property_id: id, cover: true).first
+    if cover
+      return cover
+    end
+    false
+  end
+
 end
